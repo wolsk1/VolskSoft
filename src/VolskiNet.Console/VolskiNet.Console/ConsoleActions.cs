92e6ed940b6334978c5c3e633c05362f2cf7867c
Imants Volkovs <imants.volkovs@if.lv>
2017-09-13 10:17:00 +0300

    Added console actions

---
+++ src/VolskiNet.Console/VolskiNet.Console/ConsoleActions.cs
@@ -0,0 +1,37 @@
+﻿namespace VolskiNet.Console
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class ConsoleActions
+    {
+        public static ConsoleKey SelectKey(
+            IList<KeyDescription> desiredKeys,
+            string promtText,
+            string errorText)
+        {
+            ConsoleKey selectedKey;
+            do
+            {
+                Console.WriteLine(promtText);
+
+                foreach (var desiredKey in desiredKeys)
+                {
+                    Console.WriteLine($"{desiredKey.Key} - {desiredKey.Description}");
+                }
+
+                selectedKey = Console.ReadKey().Key;
+                Console.Clear();
+                if (desiredKeys.Any(k => k.Key.Equals(selectedKey)))
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            } while (!desiredKeys.Any(k => k.Key.Equals(selectedKey)));
+
+            return selectedKey;
+        }
+    }
+}
---
+++ src/VolskiNet.Console/VolskiNet.Console/KeyDescription.cs
@@ -0,0 +1,10 @@
+﻿namespace VolskiNet.Console
+{
+    using System;
+
+    public class KeyDescription
+    {
+        public ConsoleKey Key { get; set; }
+        public string Description { get; set; }
+    }
+}

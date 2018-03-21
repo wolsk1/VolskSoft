ac131a7e0f39abcf80dc1b0e65ea1d58994de9fa
Imants Volkovs <imants.volkovs@gmail.com>
2018-03-21 20:27:27 +0200

    Update and rename build.ps1 to build.bat

---
+++ src/build.bat
@@ -0,0 +1,13 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" VolskNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
--- src/build.ps1
+++
@@ -1,6 +0,0 @@
-MyGet-Build-Solution -sln VolskNet.sln
-#mkdir Build
-#mkdir Build\lib
-#mkdir Build\lib\net45
-
-#%nuget% pack "src\VolskNet.Core\VolskNet.Core.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

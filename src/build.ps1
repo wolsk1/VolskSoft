506ce2243624953d7d33dc142d1574f5e3c286fa
Imants Volkovs <imants.volkovs@gmail.com>
2018-03-21 20:22:28 +0200

    Update build.ps1

--- src/build.ps1
+++ src/build.ps1
@@ -1,23 +1,6 @@
-@echo Off
-set config=%1
-if "%config%" == "" (
-   set config=Release
-)
- 
-set version=1.0.0
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
-)
+MyGet-Build-Solution -sln VolskNet.sln
+#mkdir Build
+#mkdir Build\lib
+#mkdir Build\lib\net45
 
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
-
-REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\VolskNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
-
-mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net45
-
-%nuget% pack "src\VolskNet.Core\VolskNet.Core.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+#%nuget% pack "src\VolskNet.Core\VolskNet.Core.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

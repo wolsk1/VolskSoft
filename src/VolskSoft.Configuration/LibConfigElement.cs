c0660d02a60911c34b0f2d3df514396a83c1c092
Imants Volkovs <imants.volkovs@gmail.com>
2018-05-24 09:25:54 +0300

    added config container

---
+++ src/VolskSoft.Configuration/LibConfigElement.cs
@@ -0,0 +1,10 @@
+﻿namespace VolskSoft.Bibliotheca.Configuration
+{
+    using System.Configuration;
+
+    public class LibConfigElement : ConfigurationElement
+    {
+        [ConfigurationProperty("pattern", IsRequired = true, IsKey = true)]
+        public string Pattern { get { return (string)this["pattern"]; } }
+    }
+}
